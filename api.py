047adc59b14d2981701df45b0b559efcc4e81c01
Ricardo Aliste G <39688076+Reko-Glarius@users.noreply.github.com>
2020-06-10 17:32:41 -0400

    Actualizacion manejo datos de API SOAP

--- api.py
+++ api.py
@@ -2,8 +2,7 @@ import logging
 from itertools import cycle
 
 logging.basicConfig(level=logging.DEBUG)
-from spyne import Application, rpc, ServiceBase, \
-    Integer, Unicode
+from spyne import Application, rpc, ServiceBase, Integer, Unicode
 from spyne import Iterable
 from spyne.protocol.http import HttpRpc
 from spyne.protocol.json import JsonDocument
@@ -11,25 +10,113 @@ from spyne.server.wsgi import WsgiApplication
 from spyne.protocol.soap import Soap11
 from spyne.model.primitive import String
 
+
 import openpyxl
 import base64
 
+def mayor_p(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12):
+    lista=[c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12]
+    m=0
+    p=0
+    for i in range(0,12):
+        if(ista[i]>m):
+            m=lista[i]
+            p=i
+    return p
+    
+
+def funcion():
+    pass
+
 class psuService(ServiceBase):
-    @rpc(Unicode, Unicode, _returns = Iterable(Unicode))
-    def separacion(ctx, dato_64):
+    @rpc(Unicode, Unicode, Unicode, _returns = Iterable(Unicode))
+    def separacion(ctx, nombre_archivo, mime, dato_64):
+        mime=mime.upper()
+        if(mime!="CSV" and mime!="TEXT" and mime!="TXT"):
+            return ("Tipo MIME especificado invalido; favor enviar especificacion como CSV, TEXT, TXT")
         carreras=[]
         for i in range(0,28):
             carreras.append([])
-        
+
         base64_bytes=dato_64.encode('ascii')
         message_bytes=base64.b64decode(base64_bytes)
         message=message_bytes.decode('ascii')
+
+
         message=message.split('\n')
         for i in range(0,len(message)):
             message[i]=message[i].split(';')
-        
+            nem=int(message[i][1])
+            ranking=int(message[i][2])
+            lenguaje=int(message[i][3])
+            matematicas=int(message[i][4])
+            ciencias=int(message[i][5])
+            historia=int(message[i][6])
 
-
+            c1=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c2=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c3=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c4_7=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c8=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c9_10=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c11=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c12_13=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c14_15=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c16_17=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c18=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)
+            c19_28=float(nem*0.1+ranking*0.1+lenguaje*0.1+matematicas*0.1)           
+            
+            if(historia>=ciencias):
+                c1=c1+float(historia*0.1)
+                c2=c2+float(historia*0.1)
+                c3=c3+float(historia*0.1)
+                c4_7=c4_7+float(historia*0.1)
+                c8=c8+float(historia*0.1)
+                c9_10=c9_10+float(historia*0.1)
+                c11=c11+float(historia*0.1)
+                c12_13=c12_12+float(historia*0.1)
+                c14_15=c14_15+float(historia*0.1)
+                c16_17=c16_17+float(historia*0.1)
+                c18=c18+float(historia*0.1)
+                c19_28=c19_28+float(historia*0.1)
+            else:
+                c1=c1+float(ciencias*0.1)
+                c2=c2+float(ciencias*0.1)
+                c3=c3+float(ciencias*0.1)
+                c4_7=c4_7+float(ciencias*0.1)
+                c8=c8+float(ciencias*0.1)
+                c9_10=c9_10+float(ciencias*0.1)
+                c11=c11+float(ciencias*0.1)
+                c12_13=c12_12+float(ciencias*0.1)
+                c14_15=c14_15+float(ciencias*0.1)
+                c16_17=c16_17+float(ciencias*0.1)
+                c18=c18+float(ciencias*0.1)
+                c19_28=c19_28+float(ciencias*0.1)
+            mayor=mayor_p(c1,c2,c3,c4_7,c8,c9_10,c11,c12_13,c14_15,c16_17,c18,c19_28)
+            if(mayor==0):
+                pass
+            elif(mayor==1):
+                pass
+            elif(mayor==2):
+                pass
+            elif(mayor==3):
+                pass
+            elif(mayor==4):
+                pass
+            elif(mayor==5):
+                pass
+            elif(mayor==6):
+                pass
+            elif(mayor==7):
+                pass
+            elif(mayor==8):
+                pass
+            elif(mayor==9):
+                pass
+            elif(mayor==11):
+                pass
+            elif(mayor==12):
+                pass
         wb=Workbook()
         ws=wb.active
         ws.title="C. 1"
@@ -62,6 +149,8 @@ class psuService(ServiceBase):
         ws27 = wb.create_sheet("C. 28")
 
         wb.save(filename = 'admitidos.xlsx')
+        yield("admitidos")
+        yield("spreadsheet")
         yield(excel_base64)
 
 class digitoService(ServiceBase):
@@ -91,6 +180,7 @@ class nompropService(ServiceBase):
             sex = 'Sra. '
         else:
             sex = 'Sr. '
+        yield ("Holi")
         yield (sex + ' ' + nomComProp )
 
 
@@ -113,4 +203,5 @@ if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     wsgi_app = WsgiApplication(application)
     server = make_server('127.0.0.1', 8000, wsgi_app)
+    print("\n\tServidor en Linea")
     server.serve_forever()
